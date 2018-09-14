0746c301e4e473db6bf2cf8d6c9abce07715b2c8
Brandy <brandy.dandy@live.com>
2018-09-14 10:18:45 -0700

    Created linear gradient for randomized colors in body

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ let val1 = generateRandomNumber().toString();
 let val2 = generateRandomNumber().toString();
 let val3 = generateRandomNumber().toString();
 
-let changeColor = function(){
-    document.body.style.background = "rgb(" + val1 + "," + val2 + "," + val3 + ")";
-}
+let val4 = generateRandomNumber().toString();
+let val5 = generateRandomNumber().toString();
+let val6 = generateRandomNumber().toString();
 
+let changeColor = function(){
+    document.body.style.background = 
+        "linear-gradient(rgb(" + val1 + "," + val2 + "," + val3 + "),rgb(" 
+        + val4 + "," + val5 + "," + val6 +"))";
+}
+    
 changeColor();
 
 
