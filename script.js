e6d164d5f54f925bb8c660b34f4ca35c8a3919a7
Brandy <brandy.dandy@live.com>
2018-09-14 14:34:07 -0700

    Added event listener to button to generate random color

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 let generateRandomNumber = function(){
-    return Math.floor(Math.random() * 256);
+    return Math.floor(Math.random() * 256).toString();
 }
-
-let val1 = generateRandomNumber().toString();
-let val2 = generateRandomNumber().toString();
-let val3 = generateRandomNumber().toString();
-
-let val4 = generateRandomNumber().toString();
-let val5 = generateRandomNumber().toString();
-let val6 = generateRandomNumber().toString();
 
 let changeColor = function(){
     document.body.style.background = 
-        "linear-gradient(rgb(" + val1 + "," + val2 + "," + val3 + "),rgb(" 
-        + val4 + "," + val5 + "," + val6 +"))";
+        "linear-gradient(rgb(" + generateRandomNumber() + "," + generateRandomNumber() + "," + generateRandomNumber() + "),rgb(" 
+        + generateRandomNumber() + "," + generateRandomNumber() + "," + generateRandomNumber() +"))";
 }
-    
-changeColor();
+
+document.getElementById("button").addEventListener("click", changeColor);
 
 
