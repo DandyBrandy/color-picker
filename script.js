e57018460264d9557797d40bc8d740fe7ca29461
Brandy <brandy.dandy@live.com>
2018-09-14 09:49:40 -0700

    Changed js to make background color of body change instead of button, removed unnecessary p tag

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let val2 = generateRandomNumber().toString();
 let val3 = generateRandomNumber().toString();
 
 let changeColor = function(){
-    document.getElementById("buttonColor").style.background = "rgb(" + val1 + "," + val2 + "," + val3 + ")";
+    document.body.style.background = "rgb(" + val1 + "," + val2 + "," + val3 + ")";
 }
 
 changeColor();
