2fde187dd8542a29bab3fcea35e9bccc5f8c6284
Brandy <brandy.dandy@live.com>
2018-09-17 09:40:13 -0700

    Added function to change text color to be more readable depending on background color

--- script.js
+++ script.js
@@ -2,12 +2,35 @@ let generateRandomNumber = function(){
     return Math.floor(Math.random() * 256).toString();
 }
 
+let topA;
+let topB;
+let topC;
+let bottomA;
+let bottomB;
+let bottomC;
+
 let createRGB = function(){
-    return "linear-gradient(rgb(" + generateRandomNumber() + "," + generateRandomNumber() + "," + generateRandomNumber() + "),rgb(" 
-    + generateRandomNumber() + "," + generateRandomNumber() + "," + generateRandomNumber() +"))";
+    bottomA = generateRandomNumber();
+    bottomB = generateRandomNumber();
+    bottomC = generateRandomNumber();
+    topA = generateRandomNumber();
+    topB = generateRandomNumber();
+    topC = generateRandomNumber();
+
+    return "linear-gradient(rgb(" + topA + "," + topB + "," + topC + "),rgb(" 
+    + bottomA + "," + bottomB + "," + bottomC +"))";
+}
+
+let changeFontColor = function(){
+    if(Number(topA) > 150){
+        document.getElementById("text").style.color = "rgb(0,0,0)";
+    } else {
+        document.getElementById("text").style.color = "rgb(255,255,255)";
+    }
 }
 
 let changeColor = function(){
+    changeFontColor();
     document.body.style.background = createRGB();
 }
 
