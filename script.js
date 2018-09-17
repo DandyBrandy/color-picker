4f4d2d67a0f8ef7ef44995659d41ba82097b7a43
Brandy <brandy.dandy@live.com>
2018-09-17 09:18:52 -0700

    Cleaned up js functions and added one to return string RGB value, added first load background color to body

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ let generateRandomNumber = function(){
     return Math.floor(Math.random() * 256).toString();
 }
 
+let createRGB = function(){
+    return "linear-gradient(rgb(" + generateRandomNumber() + "," + generateRandomNumber() + "," + generateRandomNumber() + "),rgb(" 
+    + generateRandomNumber() + "," + generateRandomNumber() + "," + generateRandomNumber() +"))";
+}
+
 let changeColor = function(){
-    document.body.style.background = 
-        "linear-gradient(rgb(" + generateRandomNumber() + "," + generateRandomNumber() + "," + generateRandomNumber() + "),rgb(" 
-        + generateRandomNumber() + "," + generateRandomNumber() + "," + generateRandomNumber() +"))";
+    document.body.style.background = createRGB();
 }
 
 document.getElementById("button").addEventListener("click", changeColor);
